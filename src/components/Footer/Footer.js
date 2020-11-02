2c5b5d41c05035ec85d5437d9acb8c4137abaaa6
learnbi <73713657+learnbi@users.noreply.github.com>
2020-11-02 15:17:37 +0700

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,9 +9,9 @@ const Footer = props => {
   return (
     <React.Fragment>
       <footer className="footer">
-        <a href="https://www.github.com/baobabKoodaa/blog/">
+        <div>
           This blog is open source. Last updated {buildTime}.
-        </a>
+        </div>
         
       </footer> 
 
