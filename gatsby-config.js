3e1419be8a8f973416c5b3cd974c348ba55dbb6a
learnbi <73713657+learnbi@users.noreply.github.com>
2020-11-02 12:51:11 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -191,28 +191,18 @@ module.exports = {
             type: "image/png"
           },
           {
-            src: "/icons/icon-96x96.png",
-            sizes: "96x96",
-            type: "image/png"
-          },
-          {
             src: "/icons/icon-144x144.png",
             sizes: "144x144",
             type: "image/png"
           },
           {
-            src: "/icons/icon-192x192.png",
-            sizes: "192x192",
+            src: "/icons/icon-196x196.png",
+            sizes: "196x196",
             type: "image/png"
           },
           {
             src: "/icons/icon-256x256.png",
             sizes: "256x256",
-            type: "image/png"
-          },
-          {
-            src: "/icons/icon-384x384.png",
-            sizes: "384x384",
             type: "image/png"
           },
           {
